77c20856b11795068bfab33d7c736c6254ea083d
Xavier Denis <xldenis@gmail.com>
2014-09-20 05:41:55 -0400

    Fix up instagram worker and add sidekiq view

--- app/workers/instagram_worker.rb
+++ app/workers/instagram_worker.rb
@@ -1,13 +1,14 @@
 class InstagramWorker
 	include Sidekiq::Worker
 
-	def perform(min, max, lat, long, access_token)
+	def perform(lat, long, min, max)
 	  client = Instagram.client(client_secret: ENV['instagram_client_secret'], client_id: ENV['instagram_client_id'])
-    client.media_search(min, max, lat, long).each do |media|
+    client.media_search(lat, long, {min_timestamp: min, max_timestamp: max}).each do |media|
+      pos = media.location
     	Gram.create(
-    		lonlat: media_item.location,
-    		body: 	media_item.caption,
-    		likes: 	media_item.likes
+    		lonlat: Gram.rgeo_factory_for_column(:lonlat).point(pos.longitude, pos.latitude),
+    		body: 	media.caption ? media.caption.text : '',
+    		likes: 	media.likes.size
       )
     end
 	end
