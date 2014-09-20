f33c78765cce60b8593597fab1eff59d8a8eee16
Jason Larigakis <jlarigakis@gmail.com>
2014-09-20 00:10:28 -0400

    rails scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ScenicRouting::Application.config.secret_key_base = '471d57478406d163c000aabf89d001111e31d7e0ffb688980f22fc0837126e56bdef31fb24dff961d2572fd46e9c11409bdc85d19de701fb8814aa87388be5ae'
