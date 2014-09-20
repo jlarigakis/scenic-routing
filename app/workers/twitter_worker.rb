2b16748dc2c12dc86f6c68d014dc9912b8498e18
Sami Jaber <jabersami@gmail.com>
2014-09-20 05:42:01 -0400

    added twitter worker

--- app/workers/twitter_worker.rb
+++ app/workers/twitter_worker.rb
@@ -3,7 +3,7 @@ class TwitterWorker
 
   def perform(min, max, lat, long, access_token)
     # client = Twitter.client(access_token: access_token)
-    client.search("", :geocode => lat + "," + long + ",1mi").each do |tweet|
+    client.search("", :geocode => lat + "," + long + ",1mi", :result_type => "popular").each do |tweet|
       Twitter.create 
         lonlat:     tweet.coordinates #TO-DO COORDINATES HERE
         body:       tweet.text,
