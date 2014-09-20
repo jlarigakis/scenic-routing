8a3bab617054fd69a39e6f4dfcd1c034ecb182bb
Xavier Denis <xldenis@gmail.com>
2014-09-20 05:19:18 -0400

    Fix a few syntax errors

--- app/workers/instagram_worker.rb
+++ app/workers/instagram_worker.rb
@@ -2,12 +2,13 @@ class InstagramWorker
 	include Sidekiq::Worker
 
 	def perform(min, max, lat, long, access_token)
-	  client = Instagram.client(access_token: access_token)
+	  client = Instagram.client(client_secret: ENV['instagram_client_secret'], client_id: ENV['instagram_client_id'])
     client.media_search(min, max, lat, long).each do |media|
-    	Instagram.new 
+    	Gram.create(
     		lonlat: media_item.location,
     		body: 	media_item.caption,
     		likes: 	media_item.likes
+      )
     end
 	end
 end
